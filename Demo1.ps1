33b9ebca060a1a521b5a0e5c47d1669755a5ac6a
HidvegiAndras <hidvegi.andras@qtc-kft.com>
2023-09-07 10:58:32 +0200

    rework

--- Demo1.ps1
+++ Demo1.ps1
@@ -1,3 +1,21 @@
 ﻿$choices = @("K","P","O")
 $rndindex = Get-Random -Minimum 0 -Maximum 3
 $choices[$rndindex]
+
+
+
+
+
+# - kérje be mit választ a játékos
+# összehasonlítani a gép választásával / elödnti
+# végtelenített formában, kilépési lehetőség
+
+if (!$false) {
+ Write-Host "igaz"
+}
+else {
+    Write-Host "hamis"
+}
+
+# -eq -ne -gt -lt -ge -le, -not -and -or -like 
+
